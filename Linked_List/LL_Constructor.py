b3777c97df54ebb4b1fcc4d8c3079482e0851060
AqibNiazi <aqibjaved5201@gmail.com>
2025-11-17 11:12:37 +0500

    LL Constructor revised

--- Linked_List/LL_Constructor.py
+++ Linked_List/LL_Constructor.py
@@ -1,21 +1,21 @@
-from typing import Any
-
-
-class Node:
+class Node: 
     def __init__(self,value):
         self.value=value
         self.next=None
-
+        
 class LinkedList:
     def __init__(self,value):
         new_node=Node(value)
         self.head=new_node
         self.tail=new_node
         self.length=1
-        
-linked_list=LinkedList(5)
 
-print(linked_list.head.value) # 5
-print(linked_list.tail.value) # 5   
+
+listed_list=LinkedList(10)
+print(listed_list.head.value) # 10
+print(listed_list.tail.value) # 10
+
+
+
 
 
